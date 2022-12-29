680d6346e1f8d5d145bd80cb742b24e5c5bc9ca6
Jakub Opiełka <jakub1.opielka1@interia.pl>
2022-12-29 20:59:57 +0100

    Creating the project

---
+++ src/main/java/pl/kuba/managementapp/ManagementAppApplication.java
@@ -0,0 +1,13 @@
+package pl.kuba.managementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kuba/managementapp/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kuba.managementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
