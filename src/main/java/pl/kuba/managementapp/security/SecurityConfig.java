1b780f2c7da62f372842b5884904e3de9f51c7a2
Jakub Opiełka <jakub1.opielka1@interia.pl>
2023-02-03 14:24:30 +0100

    SecurityCondig class problem fixing + css styles fixing

--- src/main/java/pl/kuba/managementapp/security/SecurityConfig.java
+++ src/main/java/pl/kuba/managementapp/security/SecurityConfig.java
@@ -1,10 +1,21 @@
 package pl.kuba.managementapp.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig {
-
-
-
+class SecurityConfig implements WebMvcConfigurer {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/img/**", "/styles/**").permitAll()
+                .anyRequest().authenticated()
+        );
+        http.formLogin(login -> login.loginPage("/login").permitAll());
+        http.csrf().disable();
+        return http.build();
+    }
 }
