a47475ef8e0a7da3779c30c604d5eafac84907d9
Jakub Opiełka <jakub1.opielka1@interia.pl>
2023-05-30 14:14:09 +0200

    Jest Marcin jest dobrze

--- src/main/java/pl/kuba/managementapp/web/HomeController.java
+++ src/main/java/pl/kuba/managementapp/web/HomeController.java
@@ -2,8 +2,6 @@ package pl.kuba.managementapp.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import pl.kuba.managementapp.Salary.SalaryService;
 
 @Controller
 public class HomeController {
